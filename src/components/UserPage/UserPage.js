fc51b0915451d218aec58154be3e02a1933feaba
Waldyn Benbenek <wben1201@gmail.com>
2019-03-14 14:16:49 -0500

    form done.

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
-const UserPage = (props) => (
+class UserPage extends Component{
+  state={}
+ render(){
+
+ return(
   <div>
     <h1 id="welcome">
-      Welcome, { props.user.username }!
+      Welcome, { this.props.user.username }!
     </h1>
-    <p>Your ID is: {props.user.id}</p>
+    <p>Your ID is: {this.props.user.id}</p>
     <LogOutButton className="log-in" />
+    <form onSubmit={this.handleSubmit}>
+<label>Item</label>
+<input type='text'placeholder='description'/>
+<input type='text'placeholder='image'/>
+<br></br>
+         <button type="submit">Add Guest</button>
+</form>
   </div>
+  
 );
-
+}
+}
 // Instead of taking everything from state, we just want the user info.
 // if you wanted you could write this code like this:
 // const mapStateToProps = ({user}) => ({ user });
